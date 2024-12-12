2c6987722ffc5a6b7fa9f2af60c29199c9ad6291
ultrasue <nastya.murlla@gmail.com>
2024-12-12 22:12:53 +0300

    add film.html without styling

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const films = [
+  {
+    id: 0,
+    title: "Марсианин 1",
+    originalTitle: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    link: "/film.html",
+    poster: "film.png",
+  },
+
+  {
+    id: 1,
+    title: "Марсианин 2",
+    originalTitle: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    link: "/film.html",
+    poster: "film.png",
+  },
+  {
+    id: 2,
+    title: "Марсианин 3",
+    originalTitle: "The Martian 2015",
+    category: "Фантастика, приключения",
+    rating: 7.8,
+    link: "/film.html",
+    poster: "film.png",
+  },
+];
